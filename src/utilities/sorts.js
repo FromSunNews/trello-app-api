ba0de646a2f3480c3c52ba2e54d277ec38fa5557
FromSunNews <phuongtu590@gmail.com>
2022-08-25 22:59:26 +0700

    update

--- src/utilities/sorts.js
+++ src/utilities/sorts.js
@@ -1,7 +1,6 @@
 // Order an array of objects based on another array order
 
-const mapOrder = (array, order, key) => {
+export const mapOrder = (array, order, key) => {
     array.sort((a, b) => order.indexOf(a[key]) - order.indexOf(b[key]))
     return array
 }
-export { mapOrder }
